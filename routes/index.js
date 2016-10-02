d5f466d6abb399203cb244482efbdb5230e23127
Johan <nordholts.johan@gmail.com>
2016-10-02 15:52:11 -0400

    1,2,4 done

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Accueil des quiz' });
+  res.render('index', { title: 'Accueil des quiz', Accueil:'Accueil', Bord:'', Instructions:'' });
 });
 
 router.get('/tableau_de_bord', function(req, res, next) {
-  res.render('tableau_de_bord', { title: 'Tableau de bord' });
+  res.render('tableau_de_bord', { title: 'Tableau de bord', Accueil:'', Bord:'Bord', Instructions:'' });
 });
 
 router.get('/instructions', function(req, res, next) {
-  res.render('instructions', { title: 'Instructions des quiz' });
+  res.render('instructions', { title: 'Instructions des quiz', Accueil:'', Bord:'', Instructions:'Instructions' });
 });
 
 router.get('/examen1', function(req, res, next) {
@@ -33,5 +33,16 @@ router.get('/test_rapide1', function(req, res, next) {
 router.get('/test_rapide2', function(req, res, next) {
   res.render('test_rapide2', { title: 'Test Rapide' });
 });
+
+/*router.get('/bord', function(req, res, next) {
+  res.render('bord', { title: 'Tableau de bord', Accueil:'', Bord:'Bord', Instructions:'' });
+});
+router.get('/resultats', function(req, res, next) {
+  res.render('resultats', { title: 'Résultats' });
+});
+
+router.get('/instructions', function(req, res, next) {
+  res.render('instructions', { title: 'Instructions', Accueil:'', Bord:'', Instructions:'Instructions' });
+});*/
 
 module.exports = router;
