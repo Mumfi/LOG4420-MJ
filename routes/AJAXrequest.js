d28635c913b29974d856538756766a2823e72e02
speedouille <margaux31530@hotmail.fr>
2016-10-21 16:16:31 -0400

    question 3 bis

--- routes/AJAXrequest.js
+++ routes/AJAXrequest.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render();
+var questions = require('../public/data/BD.js');
+
+
+var numero = Math.random() * questions.length+1;
+
+
+
+router.post('./test_rapide', function(req, res, next) {
+  res.json(questions[numero]);
 });
 
-router.post('/', function(req, res, next) {
-  res.json();
-});
+
 
 //var i = Math.floor(Math.random()*11);
 
